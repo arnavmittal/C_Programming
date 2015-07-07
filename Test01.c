5979c818de93db5169bcb425294ed5c95ba6e7e5
arnavmittal <mittal13@purdue.edu>
2015-07-07 14:45:55 -0400

    Completed the program, final touches after break.

--- Test01.c
+++ Test01.c
@@ -1,5 +1,6 @@
-// This program does the job of printing a N * N 2D array counter clockwise, clockwise or just normally.
-  
+// This program does the job of making or printing a N * N 2D array counter clockwise, clockwise or just normally.
+// Currently only for counter clockwise.
+
 #include<stdlib.h>
 #include<stdio.h>
 #include<string.h>
@@ -14,15 +15,27 @@ int main()
   int i; 		// loop counter
   int j; 		// loop counter
   int length = 0; 	// length of the string
-  char input_string[] = "ABCDEFGHIJKLMNOP" ;
-  
+  char* gap= " ";	// stuffing for missing places
+  char input_string[100] = "ABCDEFGHIJKLMNOP";
+  int new_len=0;
+  int old_length=0;
   // Finding length of the string
-  length = strlen(input_string);
-  
+  old_length = strlen(input_string);
+
   // Getting input for size of array.
   printf("Enter the size of array that you want:");
   scanf("%d",&n);
   
+  // Getting input string
+  new_len = n*n - old_length;
+  for (i=0;i<new_len;i++)
+  {
+    strcat(input_string,gap);
+  }  
+ 
+  // Finding length of the string
+  length = strlen(input_string);
+
   // Initializing a 2D Array.
   char **data = malloc(n*sizeof(char*));
   for(i=0;i<n;i++)
@@ -34,9 +47,10 @@ int main()
   {
     for(j=0;j<n;j++)
     {
-      data[i][j]= '\0';
+      data[i][j]= 'X';
     }
   }
+
   print_array(data, n);
   direction(data,input_string, n, length);
 } 
@@ -58,20 +72,59 @@ void print_array(char** array, int n)
   
 void direction(char** array, char* input, int n, int length)
 { 
+  int i=0;		// loop counter variables
+  int j=0;		// loop counter variables
   int dir=0;  		// direction of travel in 2D Array
-  char gap=' ';	 	// stuffing for missing places
   int add=0;
+  int limit = n-1;
+
   /*  dir = 1 = UP
       dir = 2 = DOWN
       dir = 3 = LEFT
       dir = 4 = RIGHT  */
   
-  // Counter-Clockwise diredtion hence initialization = 3
+  // Counter-Clockwise direction
   dir=3;
-  
-  while(dir)
+  i=0;
+  j=limit;
+  while(add != length)
   {
-    
-   break; 
+    while((dir == 3) && (i >= 0) && (i <= limit) && (j >= 0) && (j <= limit) && array[i][j] == 'X')
+    {
+      array[i][j]=input[add];
+      add++;
+      j--;
+    }
+    dir = 2;
+    j++;
+    i++;
+    while((dir == 2) && (i >= 0) && (i <= limit) && (j >= 0) && (j <= limit) && array[i][j] == 'X')
+    {
+      array[i][j]=input[add];
+      add++;
+      i++;
+    }
+    dir = 4;
+    i--;
+    j++;
+    while((dir == 4) && (i >= 0) && (i < n) && (j >= 0) && (j <= limit) && array[i][j] == 'X')
+    {
+      array[i][j]=input[add];
+      add++;
+      j++;
+    }
+    dir = 1;
+    i--;
+    j--;
+    while((dir == 1) && (i > 0) && (i <= limit) && (j >= 0) && (j <= limit) && array[i][j] == 'X')
+    {
+      array[i][j]=input[add];
+      add++;
+      i--;
+    }
+    dir = 3;
+    i++;
+    j--;
   }
+  print_array(array, n);
 } 
