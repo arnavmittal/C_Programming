4f059c566c5e910ba2306bc39071b79abdebfb32
arnavmittal <mittal13@purdue.edu>
2015-07-23 10:08:19 -0400

    Recursive part is fixed. Still some errors exist.

--- Test02.c
+++ Test02.c
@@ -29,7 +29,7 @@ int main()
   // Initializing the board
   board[0][0]=board[0][2]=board[0][3]=0;
   board[0][1]=1;
-  board[1][0]=2; // NOT WORKING IN THE CASE WHEN THIS == 1. 
+  board[1][0]=1; // NOT WORKING IN THE CASE WHEN THIS == 1. 
   board[1][1]=board[1][3]=0;
   board[1][2]=1;
   board[2][0]=board[2][3]=0;
@@ -39,6 +39,7 @@ int main()
   board[3][2]=1;
   
   // Printing th board
+  printf("Initial Board print.\n");
   print_array(board);
   
   // Finding the user at location(x,y) of the board
@@ -67,21 +68,33 @@ void print_array(int** array)
 void num_moves(int** array, int x, int y, int user)
 {
   /* Move is only possible in UP, DOWN, RIGHT, LEFT directions. */
+  int done=9;
   int left=0;
   int right=0;
   int up=0;
   int down=0;
-  int left_ck = y-1>=0?1:0;
-  int right_ck = y+1>=3?1:0;
-  int up_ck = x-1>=0?1:0;
-  int down_ck = x-1>=3?1:0;
+  // INITIALLY ISNTEAD OF 2's THERE WERE 1's HERE.
+  int left_ck = y-2>=0?1:0;
+  int right_ck = y+2<=3?1:0;
+  int up_ck = x-2>=0?1:0;
+  int down_ck = x+2<=3?1:0;
+
+  printf("X:%d Y:%d CURRENT VALUE INTO NEXT MOVE\n",x,y);
+  printf("Up: %d, Down: %d, Left: %d, Right: %d\n\n",up_ck,down_ck,left_ck,right_ck);
+
   if(up_ck)
   {
      if(array[x-1][y]!=0 && array[x-1][y]!=user && x >= 0 && x < 4 && y >= 0 && y < 4)
     {
-      printf("X:%d Y:%d GOING UP\n",x,y);
-      up=1;
-      num_moves(array, x-2, y,user);
+      if(array[x-2][y]==0 && array[x-2][y]!=done && (x-2) >= 0)
+      {
+        printf("X:%d Y:%d GOING UP\n",x,y);
+        up=1;
+        array[x][y]=done;
+        print_array(array);
+        num_moves(array, x-2, y,user);
+        printf("UP is done.\n");
+      }
     }
   }
 
@@ -89,9 +102,15 @@ void num_moves(int** array, int x, int y, int user)
   {
     if(array[x+1][y]!=0 && array[x+1][y]!=user && x >= 0 && x < 4 && y >= 0 && y < 4)
     { 
-      printf("X:%d Y:%d GOING DOWN\n",x,y);
-      down=1;
-      num_moves(array, x+2, y,user);
+      if(array[x+2][y]==0 && array[x+2][y]!=done && (x+2) <= 3)
+      {
+        printf("X:%d Y:%d GOING DOWN\n",x,y);
+        down=1;
+        array[x][y]=done;
+        print_array(array);
+        num_moves(array, x+2, y,user);
+        printf("DOWN is done.\n");
+      }
     }
   }
 
@@ -99,9 +118,15 @@ void num_moves(int** array, int x, int y, int user)
   {
     if(array[x][y-1]!=0 && array[x][y-1]!=user && x >= 0 && x < 4 && y >= 0 && y < 4)
     {
-      printf("X:%d Y:%d GOING LEFT\n",x,y);
-      left=1;
-      num_moves(array, x, y-2,user);
+      if(array[x][y-2]==0 && array[x][y-2]!=done && (y-2) >= 0)
+      {
+        printf("X:%d Y:%d GOING LEFT\n",x,y);
+        left=1;
+        array[x][y]=done;
+        print_array(array);
+        num_moves(array, x, y-2,user);
+        printf("LEFT is done.\n");
+      }
     }
   }
 
@@ -109,9 +134,15 @@ void num_moves(int** array, int x, int y, int user)
   {
     if(array[x][y+1]!=0 && array[x][y+1]!=user && x >= 0 && x < 4 && y >= 0 && y < 4)
     {
-      printf("X:%d Y:%d GOING RIGHT\n",x,y);
-      right=1;
-      num_moves(array, x, y+2,user);
+      if(array[x][y+2]==0 && array[x][y+2]!=done && (y+2) <= 3)
+      {
+        printf("X:%d Y:%d GOING RIGHT\n",x,y);
+        right=1;
+        array[x][y]=done;
+        print_array(array);
+        num_moves(array, x, y+2,user);
+        printf("RIGHT is done.\n");
+      }
     }
   }
 }
